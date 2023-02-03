1e17e00b04301c220705238ed6d3ccb23b89f92b
Stevixent <chiadikavirtue@gmail.com>
2023-02-03 20:59:16 +0100

    yo

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 const quoteApiUrl = "https://api.quotable.io/GET /random?minLength=80&maxLength=100";
- 
+ const quoteSection = document.getElementById("quote");
+ const userInput = document.getElementById() 
