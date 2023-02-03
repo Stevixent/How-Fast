17406223fc1e006a9a75dd88cd0d3d70b6eac835
Stevixent <chiadikavirtue@gmail.com>
2023-02-03 21:39:49 +0100

    ok

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const quoteApiUrl = "https://api.quotable.io/GET /random?minLength=80&maxLength=100";
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
  const quoteSection = document.getElementById("quote");
  const userInput = document.getElementById("quote-input");
 
@@ -7,6 +7,22 @@ const quoteApiUrl = "https://api.quotable.io/GET /random?minLength=80&maxLength=
  let timer = "";
  let mistakes = 0;
 
+//Display random quotes
+const renderNewQuote = async () => {
+    //fetch quotes from url
+    const response = await fetch(quoteApiUrl);
+
+    //store response
+    let data = await response.json();
+
+    //access quote
+    quote = data.content;
+
+    console.log(data);
+}
+
  window.onload = () => {
-    
+    userInput.value = "";
+    document.getElementById("start-test").style.display = "block";
+    document.getElementById("stop-test").style.display = "none";
  }
